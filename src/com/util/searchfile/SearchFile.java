fad45db872877615ed903e46843d3b7d0959a2d3
Lewis-Tseng <lewistseng8@gmail.com>
2020-08-03 21:07:14 +0800

    update

--- src/com/util/searchfile/SearchFile.java
+++ src/com/util/searchfile/SearchFile.java
@@ -16,21 +16,6 @@ public class SearchFile {
 
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("請輸入資料夾路徑");
-		String filepath = sc.next().trim();
-	
-		File dir = new File(filepath);
-		if (!dir.isDirectory()) {
-			System.out.println("這不是目錄");
-		}
-
-		String contents[] = dir.list();
-		File dirarry[] = dir.listFiles();
-
-		if (contents.length == 0) {
-			System.out.println("目錄" + dir.getName() + "內無檔案");
-		}
-
 		File fileListtext = new File("./searchLogfile/putfiletext" + ".txt");
 		File searchLogtext = new File("./searchLogfile/searchLogtext" + ".txt");
 		if (!fileListtext.exists()) {
@@ -49,88 +34,113 @@ public class SearchFile {
 			}
 		}
 
-		try {                                // append = fales
-			FileWriter fw = new FileWriter(fileListtext, false);
-			BufferedWriter bw = new BufferedWriter(fw);
-			PrintWriter pw = new PrintWriter(bw);
-
-			FileWriter fw1 = new FileWriter(searchLogtext, true);
+		try { // append = fales
+			FileWriter fw1 = new FileWriter(fileListtext, false);
 			BufferedWriter bw1 = new BufferedWriter(fw1);
 			PrintWriter pw1 = new PrintWriter(bw1);
 
-			int j = 0, k = 0, l = 0;
-			boolean turnoff = true;
-			while (turnoff) {
+			FileWriter fw2 = new FileWriter(searchLogtext, true);
+			BufferedWriter bw2 = new BufferedWriter(fw2);
+			PrintWriter pw2 = new PrintWriter(bw2);
 
-				++l;
-				System.out.println();
-				System.out.println("請輸入搜尋名稱(輸入0為停止搜尋)");
-				String searchtext = sc.next().trim();
-				if (searchtext.equals("0")) {
-					turnoff = false;
-					System.out.println("結束");
+			boolean switch_a = true;
+			out: while (switch_a) {
+				System.out.println("請輸入資料夾路徑(輸入0為結束)");
+				String filepath = sc.next().trim();
+
+				if ("0".equals(filepath)) {
+					System.out.println("結束程式");
 					break;
 				}
-				if (l > 0) {
-					if (l > 1) {
-						pw1.println();
-						pw1.println();
-					}
-					// 取得現在時間
-					Calendar cal = Calendar.getInstance();
-					java.util.Date du = cal.getTime();
-					pw1.println("第" + l + "次搜尋時間 ： " + du);
-					pw1.println();
-					pw1.println();
-					pw1.flush();
+				File dir = new File(filepath);
+				if (!dir.isDirectory()) {
+					System.out.println("這不是目錄");
+					System.out.println();
+					continue out;
 				}
 
-				// 印出檔案名稱至text文字檔
-				for (int i = 0; i < contents.length; i++) {
-					if (l == 1) {
-						pw.println(++j);
-						pw.println("檔案名稱 ： " + contents[i]);
-						pw.println("檔案大小 ： " + (int) (dirarry[i].length() / Math.pow(10, 6)) + "MB");
-						pw.println();
-						pw.println("======================");
-						pw.flush();
-					}
-					// 比對是否有搜尋關鍵字
-					if (contents[i].contains(searchtext)) {
-						++k;
-						pw1.println("第" + k + "項");
-						pw1.println("檔案名稱 ： " + contents[i]);
-						pw1.println("檔案大小 ： " + (int) (dirarry[i].length() / Math.pow(10, 6)) + "MB");
-						pw1.println("======================");
-						pw1.flush();
+				String contents[] = dir.list();
+				File dirarry[] = dir.listFiles();
+				if (contents == null) {
+					System.out.println("目錄" + dir.getName() + "內無檔案");
+					System.out.println();
+					continue out;
+				}
 
-						System.out.println("第 " + k + " 項 Bingo");
-						System.out.println(contents[i]);
+				int j = 0, k = 0, l = 0;
+				boolean switch_b = true;
+				while (switch_b) {
+					++l;
+					System.out.println();
+					System.out.println("請輸入搜尋名稱(輸入0為停止搜尋此資料夾)");
+					String searchtext = sc.next().trim();
+					if ("0".equals(searchtext)) {
+						switch_b = false;
+						System.out.println("此資料夾搜尋結束");
+						System.out.println();
+						continue out;
 					}
+					if (l > 0) {
+						if (l > 1) {
+							pw2.println();
+							pw2.println();
+						}
+						// 取得現在時間
+						Calendar cal = Calendar.getInstance();
+						java.util.Date du = cal.getTime();
+						pw2.println("第" + l + "次搜尋時間 ： " + du);
+						pw2.println();
+						pw2.println();
+						pw2.flush();
+					}
+
+					// 印出檔案名稱至text文字檔
+					for (int i = 0; i < contents.length; i++) {
+						if (l == 1) {
+							pw1.println(++j);
+							pw1.println("檔案名稱 ： " + contents[i]);
+							pw1.println("檔案大小 ： " + (int) (dirarry[i].length() / Math.pow(10, 6)) + "MB");
+							pw1.println();
+							pw1.println("======================");
+							pw1.flush();
+						}
+						// 比對是否有搜尋關鍵字
+						if (contents[i].contains(searchtext)) {
+							++k;
+							pw2.println("第" + k + "項");
+							pw2.println("檔案名稱 ： " + contents[i]);
+							pw2.println("檔案大小 ： " + (int) (dirarry[i].length() / Math.pow(10, 6)) + "MB");
+							pw2.println("======================");
+							pw2.flush();
+
+							System.out.println("第 " + k + " 項 Bingo");
+							System.out.println(contents[i]);
+						}
+					}
+					if (k == 0 && switch_b == true) {
+						System.out.println("沒有此檔案");
+						pw2.println("沒有此檔案");
+						pw2.flush();
+					}
+					k = 0;
 				}
-				if (k == 0 && turnoff == true) {
-					System.out.println("沒有此檔案");
-					pw1.println("沒有此檔案");
-					pw1.flush();
-				}
-				k = 0;
+
+				pw1.println("總共" + contents.length + "個檔案");
+				pw1.flush();
+
+				sc.close();
+				pw1.close();
+				bw1.close();
+				fw1.close();
+				pw2.close();
+				bw2.close();
+				fw2.close();
+
 			}
-
-			pw.println("總共" + contents.length + "個檔案");
-			pw.flush();
-
-			sc.close();
-			pw1.close();
-			bw1.close();
-			fw1.close();
-			pw.close();
-			bw.close();
-			fw.close();
 
 		} catch (IOException e) {
 			e.printStackTrace(System.err);
 		}
-
 	}
 //	Map<String, String> map = new HashMap<String, String>();
 //	map.put("4", filepath);
